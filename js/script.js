1c8cb0a7e72ac5695fdf4e078693c89dfaff788c
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-20 16:05:17 +0200

    create a function to generate a random number

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const startGame =  document.querySelector('.start');
 startGame.addEventListener('click', function() {
 
     let levelChoiseEl = document.querySelector('.select_level');
+    // console.log(levelChoiseEl.value);
     let level = levelChoiseEl.value;
 
     let numberOfCell;
@@ -19,17 +20,17 @@ startGame.addEventListener('click', function() {
     }else{
         numberOfCell = 100;
     }
+    // console.log(numberOfCell);
     
     gridContainer.innerHTML = ''
-
 
     // creo un ciclo 'for' per inserire 100 elementi al interno del container
     for (let index = 0; index < numberOfCell; index++) {
 
         // creo un elemento 
         const squareEl = document.createElement('article');
-        // aggiungo una classe a squareEl
-        squareEl.classList.add('box',  `${level}`);
+        // aggiungo classi a squareEl
+        squareEl.classList.add('box', `${level}`);
         squareEl.append(index + 1);
         // lo stampo nel 'DOM'
         gridContainer.appendChild(squareEl);
@@ -44,4 +45,23 @@ startGame.addEventListener('click', function() {
 });
 
 
+// creo una function che generi un numero randomico tra 1 e 16
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * ((max + 1) - min)) + min;
 
+    
+}
+
+function getUniqueRandomInt(notDesiredNumber, min, max) {
+    let randomNumber;
+    let isFound = true;
+
+    while (!isFound) {
+        randomNumber = getRandomInt (min, max);
+        if(notDesiredNumber.includes(randomNumber) !== false){
+            isFound = true;
+        }
+        
+    }
+    return randomNumber;
+}
